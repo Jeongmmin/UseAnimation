8f313db23e18263df29eafd08c1d381f18883347
Jeongmmin <seo88kr@hanmail.net>
2022-04-15 19:19:00 +0900

    ✍🏻 Study Variants

--- src/App.tsx
+++ src/App.tsx
@@ -12,31 +12,63 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 30px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+  background-color: whitesmoke;
+  place-self: center; //이 것에 대해 알아보기
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const BoxVars = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const circleVars = {
+  start: { opacity: 0, y: 10 },
+  end: { opacity: 1, y: 0 },
+};
 
 function App() {
   return (
     <Wrapper>
-      {/* <Box transition={{ duration: 3 }} animate={{ borderRadius: "100px" }} /> */}
-      <Box initial={{scale: 0}} transition={{ type: "spring", delay: 0.8}} animate={{ scale: 1, rotateZ: 360 }} /> 
+      <Box variants={BoxVars} initial="start" animate="end">
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+      </Box>
     </Wrapper>
   );
 }
 
 export default App;
 
-// motion.XXX 이렇게 써야 적용된다.
-
-// animate 된 스타일 컴포넌트를 가지는 방법
 /**
- *const Box = styled(motion.div)``; 이렇게 쓰면 된다.
+ * 자식에게 자동으로 값이 들어간다 <div className="initial="start" animate="end""></div>
+ * 부모의 시작, 종료 가 자동으로 복사되니까 start end 안 써도 된다.
  *
- * initial={{scale: 0}}
- * transition={{ type: "spring"}} 기본
- * stiffness:10 뻣뻣하게해줌
- * damping:30 반동(bounce) / 0이면 계속 움직임
+ * 자식들에게 delay를 줄 수 있다. - delayChildren 사용
+ * staggerChildren - delay 순차적으로 주는 것
  * 
+ * 대부분의 요소는 CSS와 동일, x, y는 모션에만 있는 것!
  */
